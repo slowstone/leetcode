de676364ca76183f3ccb71d7cb31ea3dadae315e
slowstone <498436005@qq.com>
2018-04-11 21:04:17 +0800

    modify 565 web_test

--- Array/medium/565 Array Nesting/web_test/main.cpp
+++ Array/medium/565 Array Nesting/web_test/main.cpp
@@ -7,6 +7,24 @@ using namespace std;
 
 class Solution {
 public:
+    int arrayNesting(vector<int>& a) {
+        size_t maxsize = 0;
+        for (int i = 0; i < a.size(); i++) {
+            size_t size = 0;
+            for (int k = i; a[k] >= 0; size++) {
+                int ak = a[k];
+                a[k] = -1; // mark a[k] as visited;
+                k = ak;
+            }
+            maxsize = max(maxsize, size);
+        }
+
+        return maxsize;
+    }
+};
+
+class Solution {
+public:
     int arrayNesting(vector<int>& nums) {
         int result = 0;
         int max = nums.size();
