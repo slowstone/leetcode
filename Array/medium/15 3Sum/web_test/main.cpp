db5a13baf941ef23191de862a2d075382dd965b4
slowstone <498436005@qq.com>
2018-04-19 21:22:16 +0800

    finish 15

--- Array/medium/15 3Sum/web_test/main.cpp
+++ Array/medium/15 3Sum/web_test/main.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+    	sort(nums.begin(),nums.end());
+        vector<vector<int>> output;
+        for(int i = 0;i < nums.size();i++){
+        	if(i == 0 || (i > 0 && nums[i] != nums[i-1])){
+        		int l_index = i+1,r_index = nums.size() -1;
+        		while(l_index < r_index){
+        			if(nums[l_index] + nums[r_index] + nums[i] == 0){
+        				vector<int> tmp;
+        				tmp.push_back(nums[i]);
+        				tmp.push_back(nums[l_index]);
+        				tmp.push_back(nums[r_index]);
+        				output.push_back(tmp);
+        				while(l_index < r_index && nums[l_index] == nums[l_index + 1]){
+        					l_index++;
+        				}
+        				while(l_index < r_index && nums[r_index] == nums[r_index - 1]){
+        					r_index--;
+        				}
+        				l_index++;
+        				r_index--;
+        			}
+        			else if(nums[l_index] + nums[r_index] + nums[i] > 0){
+        				r_index--;
+        			}
+        			else
+        				l_index++;
+        		}
+        	}
+        }
+        return output;
     }
 };
