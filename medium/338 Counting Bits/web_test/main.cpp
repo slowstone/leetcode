cb12f6f2911a14ce399c81517785c3cb3b9e2d4c
slowstone <498436005@qq.com>
2018-09-27 15:48:37 +0800

    Add: 338(new web_test)

--- medium/338 Counting Bits/web_test/main.cpp
+++ medium/338 Counting Bits/web_test/main.cpp
@@ -8,3 +8,14 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res(num + 1, 0);
+        for (int i = 1; i <= num; ++i) {
+            res[i] = res[i>>1] + i%2;
+        }
+        return res;
+    }
+};
