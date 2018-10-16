325b495a492e37071109ac295319e923ec43f359
slowstone <498436005@qq.com>
2018-10-16 22:43:35 +0800

    Add:396 394 395(only readme)

---
+++ medium/lintcode 395 Coins in a Line II/me_test/main.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    /**
+     * @param values: a vector of integers
+     * @return: a boolean which equals to true if the first player will win
+     */
+    bool firstWillWin(vector<int> &values) {
+        // write your code here
+    }
+};
