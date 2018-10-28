6b1ddbbb93d1f5f7ff1b0c59de1541bf27e640f1
YanXu <498436005@qq.com>
2018-10-28 22:49:40 +0800

    Add 85(not finish)

--- hard/85 Maximal Rectangle/me_test/main.cpp
+++ hard/85 Maximal Rectangle/me_test/main.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
-        
+        int m = matrix.size();
+        if(m == 0) return 0;
+        int n = matrix[0].size();
+        if(n == 0) return 0;
+        int res = 0;
     }
 };
